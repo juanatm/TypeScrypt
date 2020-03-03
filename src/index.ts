76d06acd64de2fe308432f8f1aa997ad7b2ba2b0
Juan Blanco Perales <juanblancoperales@gmail.com>
2020-03-03 10:47:52 +0100

    input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hola TS');
+const input=document.querySelector('input');
+const ul=document.querySelector('ul');
+
+
+input.onkeypress = (e) => {
+    if(e.key==="Enter"){
+        
+        let li=document.createElement('li');
+        let div=document.createElement('div');
+        let inputbox=document.createElement('input');
+        inputbox.type='checkbox';
+        let span=document.createElement('span');
+        let icon=document.createElement('i');
+        icon.className="material-icons btn-delete";
+        icon.textContent="delete_outline";
+        li.appendChild(div);
+        div.appendChild(inputbox);
+        div.appendChild(span);
+        li.appendChild(icon);
+        ul.appendChild(li);
+        span.textContent=input.value;
+
+        input.value="";
+        
+
+    }
+
+}
